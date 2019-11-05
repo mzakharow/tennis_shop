8e31401a7608f7b4e862623870120f494320cb0a
mzakharow <zaharow@bk.ru>
2019-11-05 23:58:14 +0300

    authentication system (registration)

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='account:login'), name='logout'),
 ]
