e51507fd90bf2761244c52ce88d4ee34ec9ff3f1
mzakharow <mzakharov85@gmail.com>
2020-06-04 01:11:38 +0300

    tests_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,29 +1,64 @@
 from django.test import TestCase
 
+from shop.models import Brand, Category, CartItem, Product, Cart
 
-class YourTestClass(TestCase):
 
+class BrandTestClass(TestCase):
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
+        # Set up non-modified objects used by all test methods
+        Brand.objects.create(name='TestBrand')
+
+    def test_name_label(self):
+        brand = Brand.objects.get(id=1)
+        name = brand._meta.get_field('name').verbose_name
+        print(brand.name)
+        self.assertEquals(name, 'name')
+
+    def test_name_max_length(self):
+        brand = Brand.objects.get(id=1)
+        max_length = brand._meta.get_field('name').max_length
+        self.assertEquals(max_length, 64)
 
 
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
+class CategoryTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Category.objects.create(name='Test Category', slug='test-category')
+
+    def test_name_label(self):
+        category = Category.objects.get(id=1)
+        name = category._meta.get_field('name').verbose_name
+        print(category.name)
+        self.assertEquals(name, 'name')
+
+    def test_name_max_length(self):
+        category = Category.objects.get(id=1)
+        max_length = category._meta.get_field('name').max_length
+        self.assertEquals(max_length, 64)
+
+    def test_get_absolute_url(self):
+        category = Category.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        print(category.get_absolute_url())
+        self.assertEquals(category.get_absolute_url(), '/category/test-category')
 
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+class CartTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        category = Category.objects.create(name='Test Category', slug='test-category')
+        brand = Brand.objects.create(name='TestBrand')
+        product = Product.objects.create(title='Test Product', slug='test-product', brand=brand, category=category, price=22.03)
+        CartItem.objects.create(product=product, qty=2, item_total=44.06)
 
-
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
-
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
+    def test_add_to_cart(self):
+        cart = Cart.objects.create()
+        cart.cart_total = 44.06
+        cart.item.add(CartItem.objects.get(id=1))
+        cart.save()
+        cart.add_to_cart('test-product', qty=1)
+        print(str(cart.cart_total))
+        self.assertEquals(cart.cart_total, 66.09)
